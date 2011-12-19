1d802b5e91997d2d560ef4ce0e22c0336b09e829
brody <info@jbbs-inc.com>
2011-12-19 12:34:08 -0800

    Fixed syntax errors

---
+++ utils/tests/testjbutils.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+#--------------------------
+#@author info@jbbs-inc.com  
+#unit tests
+#--------------------------
+
+#python specific
+import unittest
+import os
+import tempfile
+import subprocess
+import shlex
+import pdb
+import string
+from collections import OrderedDict
+
+class testClassAttributes(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_import(self):
+        try:
+            import jbutils
+        except:
+            print "Error: not able to import jbutils make sure that the module in installed correctly"
+
+    def tearDown(self):
+        """
+        cleanup 
+        """
+        pass
+    
+if __name__ == '__main__':
+    unittest.main()
