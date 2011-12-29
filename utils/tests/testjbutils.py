7df72574f501cfe63c51607e7bb67ac8e9704ada
brody <info@jbbs-inc.com>
2011-12-29 09:08:21 -0800

    Adding exeution of command line runner

--- utils/tests/testjbutils.py
+++ utils/tests/testjbutils.py
@@ -13,20 +13,36 @@ import subprocess
 import shlex
 import pdb
 import string
+import sys
 
 class testClassAttributes(unittest.TestCase):
     def setUp(self):
         pass
 
     def test_import(self):
+        '''
+        import the module
+        '''
         try:
             import jbutils
         except:
             print "Error: not able to import jbutils make sure that the module in installed correctly"
     def test_main(self):
+        '''
+        execute main
+        '''
         import jbutils
         jbutils.main()
     
+    def test_commandline(self):
+        '''
+        run via the command line
+        '''
+        if sys.platform.startswith('win32'):
+            subprocess.call('jbutilsrun.bat')
+        else:
+            subprocess.call('jbutilsrun')
+
     def tearDown(self):
         """
         cleanup 
