7e58d8df162d9c9ea0475865d8e6ec09dc84cfe3
Brody <brodytwo@gmail.com>
2022-05-21 16:47:41 -0700

    updates for the time period

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM jfloff/alpine-python
+FROM python:3.8.13
 
 # Install kubectl from Docker Hub.
 COPY --from=lachlanevenson/k8s-kubectl:v1.10.3 /usr/local/bin/kubectl /usr/local/bin/kubectl
@@ -7,5 +7,5 @@ WORKDIR /opt/work
 
 COPY build.py /opt/work
 COPY utils /opt/work/utils
-
+RUN pip install --upgrade pip
 RUN python build.py -b
--- build.py
+++ build.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 '''
 entry point for deployment packaging, and automated testing
 author info@jbbs-inc.com
@@ -31,7 +33,6 @@ def log(message = "", debug = 1):
 def checkvirtualenv():
     """
     checks to make sure that virtualenv is installed
-    TODO if it is not found then install 1.6.4
     """
     try:
         import virtualenv
@@ -42,6 +43,8 @@ def checkvirtualenv():
         except:
             log("Warning: pip python module not found, Not sure what to do now.")
             exit(1)
+        cmd = "pip install --upgrade pip"
+        subprocess.call( cmd.split( " " ) )
         cmd = "pip install virtualenv"
         subprocess.call( cmd.split( " " ) )
 
@@ -76,228 +79,15 @@ def install_deps(tmpdir = "", test = False):
     cmd = ""
     if sys.platform.startswith('win32'):
         targetDir = "Scripts"
-        cmd = "%s install pymongo nose" %( os.path.join(tmpdir, targetDir, "pip") )
+        cmd = "%s install --upgrade pip" %( os.path.join(tmpdir, targetDir, "pip") )
     else:
         targetDir = "bin"
-        cmd = "%s install coverage nose" %( os.path.join(tmpdir, targetDir, "pip") )
-    log( "Installing coverage and nose into a virtualenv.")
+        cmd = "%s install --upgrade pip" %( os.path.join(tmpdir, targetDir, "pip") )
+    log( "Updating pip to the latest version")
     log( cmd )
     if not test:
         ret = subprocess.call( cmd.split( " " ) )
 
-    return ret
-def getTargetDir():
-    if sys.platform.startswith('win32'):
-        targetDir = "Scripts"
-    else:
-        targetDir = "bin"
-    return targetDir
-
-def install_modules(test = False, tmpdir = "", dep_return = "", nose = False, nose_coverage_html = "/tmp/pythonUnitCoverage"):
-    """
-    installs python modules into virtualenv
-    """
-    ret = 0
-    current_dir = os.path.abspath( os.path.curdir )
-    module = os.path.abspath( os.path.join(current_dir, "utils") )
-    targetDir = getTargetDir()
-
-    cmd = os.path.join(tmpdir, targetDir, "python")
-
-    #install the module into the virtualenv
-    install_cmd = cmd + " setup.py install"
-    if test:
-        log("\n\n %s \n\n" %(install_cmd) )
-    else:
-        os.chdir(module)
-        try:
-            ret = subprocess.call(install_cmd.split(" "))
-        except:
-            log("Error: Was not able to python setup.py install %s" %(module))
-        os.chdir(current_dir)
-
-    if not ret == 0:
-        return ret
-    else:
-        ret = run_test(test = test, tmpdir = tmpdir, dep_return = dep_return, nose = nose, nose_coverage_html = nose_coverage_html)
-    return ret
-
-def run_test(test = False, tmpdir = "", dep_return = "", nose = False, nose_coverage_html = "/tmp/pythonUnitCoverage"):
-    ret = 0
-    #the location of the python module
-    current_dir = os.path.abspath( os.path.curdir )
-    module = os.path.abspath( os.path.join(current_dir, "utils") )
-    targetDir = ""
-    if sys.platform.startswith('win32'):
-        targetDir = "Scripts"
-    else:
-        targetDir = "bin"
-
-    testfilepy = os.path.abspath( os.path.join(module, "tests") )
-    if not os.path.exists(testfilepy):
-        log("Error: tests dir (%s) does not exist" %(testfilepy))
-        return 1
-
-    if sys.platform.startswith('win32'):
-        unit_cmd = "%s -v --exe %s" %(os.path.join(tmpdir, targetDir, "nosetests"), module )
-    else:
-        if nose:
-            log("\n\nRunning unit tests\n\n")
-            #clean up old code coverage
-            if os.path.exists(nose_coverage_html):
-                cmd = "rm -rf %s" %(nose_coverage_html)
-                if test:
-                    log("\n\n %s \n\nz" %(cmd) )
-                else:
-                    subprocess.call(cmd.split(" "))
-            #create directory
-            cmd = "mkdir -p %s" %(nose_coverage_html)
-            if test:
-                log("\n\n %s \n\nz" %(cmd) )
-            else:
-                subprocess.call(cmd.split(" "))
-
-            #running through all tests in the tests directory
-            unit_cmd = "%s -v --cover-html --cover-package=jbutils --cover-tests --with-coverage --cover-erase --with-xunit --exe %s" \
-                       %( os.path.join( tmpdir, targetDir, "nosetests" ), testfilepy )
-        else:
-            unit_cmd = "%s %s" %(os.path.join(tmpdir, targetDir, "python"), testfilepy )
-
-    #TODO fix as this is kind of hacky
-    if test:
-        log("\n\n %s \n\n" %(unit_cmd) )
-    else:
-        os.chdir(module)
-        try:
-            log( unit_cmd + "\n\n" )
-            ret = subprocess.call( unit_cmd.split( " " ) )
-            #if python unit testing show coverage report
-            #if nose:
-            #    cmd = "open %s/index.html" %(nose_coverage_html)
-            #    subprocess.Popen( cmd.split(" "), stdout = subprocess.PIPE, stderr = subprocess.PIPE)
-
-        except:
-            log("Error: Unit tests at (%s) had problems" %(testfilepy) )
-        #TODO fix as this is kind of hacky
-        os.chdir(current_dir)
-
-    return ret
-
-def get_python():
-    """
-    locates the current installation of python virtual env
-    """
-    #TODO make this more robust
-    try:
-        python = os.environ["VIRTUAL_ENV"]
-    except KeyError:
-        log("Warning: Not running in local virtualenv.")
-        python = " "
-    return python
-
-def remove_module(tmpdir = "", module = ""):
-    ret = 0
-    #the location of the python module
-    current_dir = os.path.abspath( os.path.curdir )
-    targetDir = getTargetDir()
-    remove_cmd = os.path.join(tmpdir, targetDir, "pip")
-    if not os.path.exists(remove_cmd):
-        log("Error: Was not able to find pip module. Please install or activate a virtualenv and try again")
-        return 1
-    remove_cmd += " uninstall %s" %(module)
-    log("\n\n%s\n\n" %(remove_cmd) )
-    ret = subprocess.call(remove_cmd.split(' ') )
-    return ret
-
-def main():
-    """
-    main method that calls into all other methods
-    """
-    ret = 0
-    runpythonunit = False
-    runpythonunitfast = False
-    des = "Command Line Options"
-    phpunit_args = ""
-    #builds up command line arguments
-    parser = ArgumentParser(description = des)
-    parser.add_argument('--unit', '-b', dest='u', action = 'store_true', help='Full tests, run locally')
-    parser.add_argument('--unit-fast', '-f', dest='f', action = 'store_true', help='Full tests, run locally, no network connection')
-    parser.add_argument('--docker-build', '-d', dest='d', action = 'store_true', help='build with in a docker container')
-    args = parser.parse_args()
-'''
-entry point for deployment packaging, and automated testing
-author info@jbbs-inc.com
-'''
-
-#python specific
-import tempfile
-import subprocess
-import shlex
-import os
-import sys
-import pdb
-import time
-from argparse import ArgumentParser
-
-#python 3rd party
-
-#internal
-
-
-def log(message = "", debug = 1):
-    if debug == 1:
-        sys.stderr.flush()
-        sys.stdout.flush()
-        print(message)
-        sys.stderr.flush()
-        sys.stdout.flush()
-    else:
-        pass
-
-def checkvirtualenv():
-    """
-    checks to make sure that virtualenv is installed
-    """
-    try:
-        import virtualenv
-    except ImportError:
-        log("Warning: virtualenv python module not found, trying with pip")
-        try:
-            import pip
-        except:
-            log("Warning: pip python module not found, Not sure what to do now.")
-            exit(1)
-        cmd = "pip install virtualenv"
-        subprocess.call( cmd.split( " " ) )
-
-def createvirtualenv():
-    """
-    This creates a virtualenv in a temporary location
-    """
-    tmpdir = tempfile.mkdtemp()
-    vcmd = ""
-    if sys.platform.startswith('win32'):
-        vcmd = "python -m virtualenv %s" %(tmpdir)
-    else:
-        vcmd = "virtualenv %s" %(tmpdir)
-    subprocess.call( vcmd.split(" ") )
-    return tmpdir
-
-def install_deps(tmpdir = "", test = False):
-    """
-    installs dependances
-    requires an internet connection
-    TODO: if already installed skip
-    TODO: return a list of dependancies that failed to install
-    """
-    ret = 0
-    """
-    try:
-        import nose
-        import coverage
-    except:    
-    """
-    targetDir = ""
     cmd = ""
     if sys.platform.startswith('win32'):
         targetDir = "Scripts"
